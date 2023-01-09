756179f79901ad1be8b7bd3f79aa4232172c1d2d
kevin vaddoriya <kevinvaddoriya1@gmail.com>
2023-01-09 22:58:51 +0530

    binarySearch.java

--- src/binarySearch.java
+++ src/binarySearch.java
@@ -1,5 +1,26 @@
 public class binarySearch {
     public static void main(String[] args) {
-        System.out.println("kevin");
+        int[] arr = {1,4,7,10,56,90,200,890};
+        int target = 7;
+        int ans =  binaryAlgo(target,arr);
+        System.out.println(ans);
+
+    }
+    static int binaryAlgo(int target, int[] arr){
+        int start = 0;
+        int end = arr.length;
+
+        while (start<=end){
+            int mid = start + (end-start) / 2;
+
+            if (target < arr[mid]){
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid+1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
